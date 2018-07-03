615b183c5fbc4ddf0785ca1998893291e798c1fa
994759771 <994759771@qq.com>
2018-07-03 10:27:39 +0800

    Star

---
+++ AudioAnalysis/UserControl1.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Administrator
+ * Date: 2018/7/3
+ * Time: 10:25
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AudioAnalysis
+{
+	/// <summary>
+	/// Description of UserControl1.
+	/// </summary>
+	public partial class UserControl1 : UserControl
+	{
+		public UserControl1()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
